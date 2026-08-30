49a273e1c7517898f4708371ffb36a6b5c19032e
Juan.Velez29 <velezjuanjose80@gmail.com>
2026-08-30 17:29:11 -0500

    Continuación Hooks

--- src/screens/ClasesScreen.js
+++ src/screens/ClasesScreen.js
@@ -1,10 +1,12 @@
 import React, {useState} from 'react';
 import {View, Text, TextInput, FlatList, ScrollView, StyleSheet} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import { Ionicons } from '@expo/vector-icons';
 import Card from '../components/Card';
+import NivelFiltro from '../components/NivelFiltro';
 import {colors, spacing, typography} from '../theme';
 import {CLASES, NIVELES} from '../data/clases';
-import { Ionicons } from '@expo/vector-icons';
+
 
 export default function ClasesScreen ({navigation })  {
     //const {columnas, paddingHorizontal} = useResponsive();
@@ -21,9 +23,19 @@ export default function ClasesScreen ({navigation })  {
                         value={nivel}
                         onChangeText={setNivel}
                         autocorrect={false}
-                        
+                        autoComplete={false}
                     />
+                    {busqueda.length > 0 && (
+                        <Ionicons
+                        name="close-circle"
+                        size={18}
+                        onPress={() => setBusqueda('')}
+                        />
+                    )}
                 </View>
+                <ScrollView>
+                    style={flexGrow: 0}
+                </ScrollView>
             </View>
         </View>
 
